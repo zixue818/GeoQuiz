d081f9fcc6a6ea597ce239f36f24597be7e24d86
ljc <2812338@qq.com>
2018-11-07 16:43:59 +0800

    from Button to ImageButton

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,8 +14,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Toast mToast;
-    private Button mNextButton;
-    private Button mPrevButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionsBank=new Question[]{
             new Question(R.string.question_australia,true),
@@ -48,7 +49,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton=(Button)findViewById(R.id.next_button);
+        mNextButton=(ImageButton)findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,7 +65,7 @@ public class QuizActivity extends AppCompatActivity {
             }
 
         });
-        mPrevButton=(Button)findViewById(R.id.prev_button);
+        mPrevButton=(ImageButton)findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
