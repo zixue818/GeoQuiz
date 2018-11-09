718e456fc13835f492c3ce4b5e7eab42eca1f869
ljc <2812338@qq.com>
2018-11-09 21:05:18 +0800

    添加了屏幕旋转后加载另一个布局和活动重新销毁后保存数据

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String KEY_INDEX="index";
+    private static final String TAG="QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     //private Toast mToast;
@@ -31,7 +34,11 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate() called");
         setContentView(R.layout.activity_quiz);
+        if(savedInstanceState!=null){
+            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX);
+        }
         mQuestionTextView=(TextView)findViewById(R.id.question_text_view);
         updateQuestion();//show the question text
         mTrueButton=(Button)findViewById(R.id.true_button);
@@ -94,4 +101,43 @@ public class QuizActivity extends AppCompatActivity {
         }
         Toast.makeText(this,messageId,Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
+    //重写方法来观察生命周期
+
+        @Override
+        protected void onStart () {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+        @Override
+        protected void onResume () {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+        @Override
+        protected void onPause () {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+        @Override
+        protected void onStop () {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+        @Override
+        protected void onDestroy () {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+
 }
